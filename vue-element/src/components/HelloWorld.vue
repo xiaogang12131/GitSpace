25e9ad6ffc756ea01663e1b04eb314556c9191a1
xiaogang12131 <947934544@qq.com>
2018-05-28 15:45:55 +0800

    提交文件

---
+++ vue-element/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <el-tree
+      :props="props1"
+      :load="loadNode1"
+      lazy
+      show-checkbox>
+    </el-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      props1: {
+        label: 'name',
+        children: 'zones',
+        isLeaf: 'leaf'
+      },
+    };
+  },
+  methods: {
+    loadNode1(node, resolve) {
+      if (node.level === 0) {
+        return resolve([{ name: 'region' }]);
+      }
+      if (node.level > 1) return resolve([]);
+      debugger;
+      setTimeout(() => {
+        const data = [{
+          name: 'leaf',
+          leaf: true
+        }, {
+          name: 'zone'
+        }];
+
+        resolve(data);
+      }, 500);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
